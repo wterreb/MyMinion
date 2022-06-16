4b101f79bd33dd886a921c3f2a974b6f70704140
wterreb <werner.terreblanche@gmail.com>
2022-06-16 14:57:41 +1200

    Changed it to work with ulimited nr of files

--- MyMinion.ino
+++ MyMinion.ino
@@ -10,6 +10,8 @@ bool OFF = HIGH;
 #include <SD.h>
 #include <Keyboard.h> // The main library for sending keystrokes.
 
+bool  debugMode = false;
+
 const byte ROWS = 4; //four rows
 const byte COLS = 3; //three columns
 char keys[ROWS][COLS] = {
@@ -22,25 +24,31 @@ byte rowPins[ROWS] = {6,5,4,3}; //connect to the row pinouts of the keypad
 byte colPins[COLS] =  {7,8,9}; //connect to the column pinouts of the keypad
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 File myFile;
-const int NORMAL = 0;
-const int CHOOSE_FILE = 1;
-const int READ_SERIAL = 2;
-const int WRITE_FILE = 3;
-int mode = NORMAL;
-char fileNr = "\0";
+const int MODE_NORMAL = 0;
+const int MODE_CHOOSE_WRITE_FILENAME = 1;
+const int MODE_READ_FILE = 2;
+const int MODE_SHOW_SEND_SERIAL_DATA_MESSAGE = 3;
+const int MODE_READ_SERIAL_DATA = 4;
+const int MODE_ERROR = 5;
+int mode = MODE_NORMAL;
+char keyChar = "\0";
+String fileNumber = "";
 String filename = "";
-String serial_txt = "";
+
 
 void setup() {
    pinMode(LED_BUILTIN, OUTPUT);
    pinMode(LED_RED, OUTPUT);
    pinMode(LED_GRN, OUTPUT);
    pinMode(LED_BLUE, OUTPUT);
-   led_green();
+   all_leds_off();
    Serial.begin(9600);
    if (!SD.begin(10)) {
-    digitalWrite(LED_RED, ON);
+    led_red();
     while (1);
+  }
+  else {
+    led_green();
   }
   Keyboard.begin();  // Initialise the library.
 }
@@ -71,92 +79,105 @@ void led_green()
 }
 
 
-
-
 void set_filename(char key) {
   filename = key + ".txt";
 }
 
+
 void normal_mode(char key) {
-   led_green();
-  if (key >= '0' && key <= '9'){
-     filename = "";
-     filename.concat(key);
-     filename.concat(".txt");
-     myFile = SD.open(filename);  
-      if (myFile) {
-         led_blue();
-         // read from the file until there's nothing else in it:
-        String s;
-        while (myFile.available()) {
-          char c = myFile.read();
-          s += c;
-        }
-        myFile.close();
-        Keyboard.print(s);                //  Send as keyboard chars
-        Keyboard.press(KEY_RETURN);       // Press the Enter key.
-        delay(100);                       // Wait for the computer to register the press.
-        Keyboard.releaseAll();
-        led_green();
-      }
+
+  if (key >= '0' && key <= '9') {
+     filename.concat(key);  
   }
   else {
     if (key == '*') {
-       led_blue();
-       mode = CHOOSE_FILE;
+       mode = MODE_CHOOSE_WRITE_FILENAME;
     }
-    else {      
-      digitalWrite(LED_RED, ON);
-    }
+    if (key == '#') { 
+      filename.concat(".txt");
+      switch (mode) {
+          case MODE_NORMAL :      
+             mode = MODE_READ_FILE;
+             break;
+          case MODE_CHOOSE_WRITE_FILENAME : 
+              mode = MODE_SHOW_SEND_SERIAL_DATA_MESSAGE;
+              break;
+          default:
+             mode = MODE_ERROR;   
+             break;
+      }
+    }  
   }
 }
 
-
-void read_serial() {
-  char inchar = '\0';
-  
-  if (Serial.available()) { 
-    delay(100);
-    serial_txt = Serial.readString();
-  }
-  if (serial_txt.length() > 0) {
-    mode = WRITE_FILE;
-  }
-}
-
-bool writefile() {
-    bool retval = false;
-    filename = "";
-    filename.concat(fileNr);
-    filename.concat(".txt");
+void read_file_contents() {
+  myFile = SD.open(filename);  
+  if (myFile) {
+     
+    if (debugMode) {
+      Serial.print("Read file "); Serial.println(filename);
+    }
     
-    if (SD.exists(filename)) {
-       SD.remove(filename);
+    while (myFile.available()) {  // read from the file until there's nothing else in it:
+      char c = (char)myFile.read();
+       if (debugMode) {
+          Serial.print(c);
+       } else {    
+          Keyboard.print(c);                //  Send as keyboard chars
+       }
     }
-    myFile = SD.open(filename, FILE_WRITE);  
-    if (myFile) {
-      myFile.println(serial_txt);
-      myFile.close();  
+    myFile.close();
+    if (debugMode) {
+    Serial.println(" ");
+    } else {
+        Keyboard.press(KEY_RETURN);       // Press the Enter key.
+        delay(100);                       // Wait for the computer to register the press.
+        Keyboard.releaseAll();
     }
-    if ( SD.exists(filename) ) {
-       retval = true;
-    }
-    return retval;
+  } else {       
+      led_red();
+      delay(1000); 
+  }
+  filename = "";  
 }
+
+
+bool read_serial() {
+  bool retval = false;
+  if ( Serial.available() ) { 
+      if (SD.exists(filename)) {
+          SD.remove(filename);
+      }
+      Serial.print("Write file : "); Serial.println(filename);
+      myFile = SD.open(filename, FILE_WRITE);
+      while ( Serial.available() )
+      {
+        char rc = Serial.read();
+        if (myFile) {
+          myFile.print(rc);
+        }
+        Serial.print(rc);
+      }
+      if (myFile) {
+          myFile.close();  
+      }
+      if ( SD.exists(filename) ) {
+          retval = true;
+      }
+  }
+  return retval;
+}
+
+
 
 void loop() {
   char key = keypad.getKey();
 
   if (key != '\0') {
       switch (mode) {
-      case NORMAL : 
+      case MODE_NORMAL :               // Fall through
+      case MODE_CHOOSE_WRITE_FILENAME:
              normal_mode(key);
-             break;
-      case CHOOSE_FILE :  
-             fileNr = key; 
-             mode = READ_SERIAL;
-             serial_txt = "";
-             Serial.print("Send contents for file '");  Serial.print(fileNr); Serial.println(".TXT' via serial port");
              break;
        default :
            led_red(); 
@@ -165,19 +186,35 @@ void loop() {
     }
     else {
       switch (mode) {
-         case READ_SERIAL :
-              read_serial(); 
-              break;
-         case WRITE_FILE :
-              Serial.print("Writing contents to file '");  Serial.print(fileNr); Serial.println(".TXT'");
-              if ( writefile() ) {
-                 mode = NORMAL;
+         case MODE_READ_FILE: 
+             read_file_contents();
+             mode = MODE_NORMAL;
+             break;
+
+         case MODE_SHOW_SEND_SERIAL_DATA_MESSAGE:
+             Serial.println("");
+             Serial.print("Send contents for file '");  Serial.print(filename); Serial.println("' via serial port"); 
+             mode = MODE_READ_SERIAL_DATA;
+             break;
+             
+         case MODE_READ_SERIAL_DATA :
+              if ( read_serial() ) {
+                 mode = MODE_NORMAL;
+                 filename = "";
               }
-              else {
-                led_red();
-              }   
-              mode = NORMAL; 
               break;
       }
     }   
+    if (mode == MODE_ERROR) {
+      led_red();
+      delay(1000); 
+      mode = MODE_NORMAL;
+    }
+    if (mode == MODE_NORMAL || mode == MODE_CHOOSE_WRITE_FILENAME) {
+      if (filename.length() == 0) {
+         led_green();
+      } else {
+         led_blue();
+      }
+    }
 }
